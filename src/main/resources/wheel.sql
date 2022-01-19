d171097a7f83ed711ce0c0e3e4eba95127396111
Emil Ankerstjerne <emil.ankerstjerne@gmail.com>
2022-01-19 13:28:43 +0100

    Updated Field class attribute fieldValue to field_value and corrected in all relations.

--- src/main/resources/wheel.sql
+++ src/main/resources/wheel.sql
@@ -1,11 +1,11 @@
--- DROP DATABASE  IF EXISTS wheelOfFortune;
+DROP DATABASE  IF EXISTS wheelOfFortune;
 CREATE Database wheelOfFortune;
 use wheelOfFortune;
 
 -- Uncomment below statements if you already have the DB created and want to reset it.
--- DROP TABLE IF EXISTS winner;
--- DROP TABLE IF EXISTS field;
--- DROP TABLE IF EXISTS wheel_of_fortune;
+DROP TABLE IF EXISTS winner;
+DROP TABLE IF EXISTS field;
+DROP TABLE IF EXISTS wheel_of_fortune;
 
 CREATE TABLE wheel_of_fortune
 (
@@ -19,7 +19,7 @@ CREATE TABLE field
 (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name varchar(255),
-    fieldValue int,
+    field_value int,
     wheel_id int,
     FOREIGN KEY (wheel_id) REFERENCES wheel_of_fortune (id)
 );
@@ -35,9 +35,9 @@ CREATE TABLE winner
 );
 
 INSERT INTO wheel_of_fortune (name) value ('Wheel of Fortune');
-INSERT INTO field (name, fieldValue, wheel_id) VALUES ('Washing Machine', 3500, 1);
-INSERT INTO field (name, fieldValue, wheel_id) VALUES ('Security Camera', 1500, 1);
-INSERT INTO field (name, fieldValue, wheel_id) VALUES ('Electric Shaver', 2000, 1);
-INSERT INTO field (name, fieldValue, wheel_id) VALUES ('PC Monitor', 2500, 1);
-INSERT INTO field (name, fieldValue, wheel_id) VALUES ('Fridge', 3000, 1);
-INSERT INTO field (name, fieldValue, wheel_id) VALUES ('Dud', 0, 1);
+INSERT INTO field (name, field_value, wheel_id) VALUES ('Washing Machine', 3500, 1);
+INSERT INTO field (name, field_value, wheel_id) VALUES ('Security Camera', 1500, 1);
+INSERT INTO field (name, field_value, wheel_id) VALUES ('Electric Shaver', 2000, 1);
+INSERT INTO field (name, field_value, wheel_id) VALUES ('PC Monitor', 2500, 1);
+INSERT INTO field (name, field_value, wheel_id) VALUES ('Fridge', 3000, 1);
+INSERT INTO field (name, field_value, wheel_id) VALUES ('Dud', 0, 1);
