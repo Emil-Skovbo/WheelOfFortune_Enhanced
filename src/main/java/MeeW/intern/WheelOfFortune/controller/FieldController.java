854ef1c224e8fe161ffec1c13686cd8bf0ed570a
Jack <cph-jj484@cphbusiness.dk>
2022-01-19 12:25:30 +0100

    FieldController and FieldRepository added

---
+++ src/main/java/MeeW/intern/WheelOfFortune/controller/FieldController.java
@@ -0,0 +1,45 @@
+package MeeW.intern.WheelOfFortune.controller;
+
+import MeeW.intern.WheelOfFortune.entities.Field;
+import MeeW.intern.WheelOfFortune.repository.FieldRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+public class FieldController {
+
+    @Autowired
+    FieldRepository repo;
+
+    public List<Field> retrieveAllFields(){
+        if (repo.findAll().isEmpty()){
+            throw new RuntimeException();
+        }
+        return repo.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public EntityModel<Field> retrieveField(@PathVariable int id){
+        Optional<Field> field = repo.findById(id);
+        if (field.isEmpty())
+            throw new FieldNotFoundException();
+
+        EntityModel<Field> resource = EntityModel.of(field.get());
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllFields());
+        resource.add(linkTo.withRel("all-fields"));
+
+        Link selfLink = linkTo(methodOn(this.getClass()).retrieveField(id)).withSelfRel();
+        resource.add(selfLink);
+
+        return resource;
+    }
+}
---
+++ src/main/java/MeeW/intern/WheelOfFortune/repository/FieldRepository.java
@@ -0,0 +1,7 @@
+package MeeW.intern.WheelOfFortune.repository;
+
+import MeeW.intern.WheelOfFortune.entities.Field;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FieldRepository extends JpaRepository<Field, Integer> {
+}
