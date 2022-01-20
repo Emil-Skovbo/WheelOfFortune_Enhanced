f9edc22e74b62914963f9772f9a32103e8b8f7d8
Emil <emilskovbo18@gmail.com>
2022-01-20 09:26:11 +0100

    work on tests. need to help Noell

--- src/test/java/MeeW/intern/WheelOfFortune/WheelOfFortuneApplicationTests.java
+++ src/test/java/MeeW/intern/WheelOfFortune/WheelOfFortuneApplicationTests.java
@@ -1,13 +1,24 @@
 package MeeW.intern.WheelOfFortune;
 
+import MeeW.intern.WheelOfFortune.controller.WheelOfFortuneController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class WheelOfFortuneApplicationTests {
 
+    @Autowired
+    private WheelOfFortuneController controller;
+
+
     @Test
     void contextLoads() {
+        assertThat(controller).isNotNull();
+        assertThat(controller).isNotNull();
+
     }
 
 }
