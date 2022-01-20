f7242b266ac4c012d7c1a6c6885fccc6f45368b0
Jack <cph-jj484@cphbusiness.dk>
2022-01-20 11:51:06 +0100

    Updated merge error fix

--- src/main/java/MeeW/intern/WheelOfFortune/controller/WinnerController.java
+++ src/main/java/MeeW/intern/WheelOfFortune/controller/WinnerController.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+@CrossOrigin(origins = "*")
 
 @RestController
 @RequestMapping("/winner")
