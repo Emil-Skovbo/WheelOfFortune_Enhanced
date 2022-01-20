5c7aeb6c9b9d22a321b4f928209569e04df801b4
Emil Ankerstjerne <emil.ankerstjerne@gmail.com>
2022-01-20 19:23:07 +0100

    virker som backend

--- src/main/java/MeeW/intern/WheelOfFortune/WheelOfFortuneApplication.java
+++ src/main/java/MeeW/intern/WheelOfFortune/WheelOfFortuneApplication.java
@@ -2,11 +2,34 @@ package MeeW.intern.WheelOfFortune;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class WheelOfFortuneApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WheelOfFortuneApplication.class, args);
+    }
+
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
+
+    @Configuration
+    public class WebConfig extends WebMvcConfigurerAdapter {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+        }
     }
 }
